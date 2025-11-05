9da4f5f1d0071351e921738d59d21bc3c0b76f32
dev-null-loop <dev.null.loop@gmail.com>
2025-11-05 15:40:49 +0200

    -provider

--- table/provider.tf
+++
@@ -1,5 +0,0 @@
-# provider "oci" {
-#   tenancy_ocid        = "ocid1.tenancy.oc1..aaaaaaaafvbrqwizb2l62d7o46h622ibhfp2at56cfjxak7x3jqgh42ligrq"
-#   region              = "us-chicago-1"
-#   config_file_profile = "DEFAULT"
-# }
